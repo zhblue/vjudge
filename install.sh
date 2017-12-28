36b2dd8792fdd17c21ffd15cca20eae8de906d37
张浩斌 <newsclan@gmail.com>
2017-12-28 15:41:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@ apt-get update
 apt-get -y install tomcat7 mysql-server
 wget https://github.com/zhblue/vjudge/raw/master/vjudge.war
 cp vjudge.war /var/lib/tomcat7/webapps/
-echo "waiting for tomcat7 deploying vjudge.war ... 5s count down"
-sleep 5
+echo "waiting for tomcat7 deploying vjudge.war ... 8s count down"
+sleep 8
 DBUSER=`cat /etc/mysql/debian.cnf | grep user|awk '{print $3}'|head -1`
 DBPASS=`cat /etc/mysql/debian.cnf | grep password|awk '{print $3}'|head -1`
 SQL=`find /var/lib/tomcat7/webapps/vjudge -name "*.sql"`
 cat $SQL | mysql -u$DBUSER -p$DBPASS
 cd /var/lib/tomcat7/webapps/vjudge/WEB-INF/classes
 sed -i "s/jdbc.username=                 root/jdbc.username=$DBUSER/g" config.properties
-sed -i "s/jdbc.password=/jdbc.password=$DBPASS/g" config.properties
+sed -i "s/jdbc.password= /jdbc.password=$DBPASS/g" config.properties
 
 service tomcat7 restart
